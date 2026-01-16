af23c90c974ef5316d67a292fe82f7cf1d97d548
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-16 15:34:31 -0800

    added items 11-20 and numbered the list

--- COMP003A.Assignment1-3/COMP003A.Assignment1-3/Program.cs
+++ COMP003A.Assignment1-3/COMP003A.Assignment1-3/Program.cs
@@ -7,16 +7,26 @@ namespace COMP003A.Assignment1_3
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me");
-            Console.WriteLine("I am a senior in highschool.");
-            Console.WriteLine("I am 18 years old.");
-            Console.WriteLine("My ethnicity is Filipino.");
-            Console.WriteLine("My favorite academic subject is math.");
-            Console.WriteLine("I like to play videogames.");
-            Console.WriteLine("My favorite artist is Bruno Mars.");
-            Console.WriteLine("I enjoy playing volleyball.");
-            Console.WriteLine("I also enjoy watching and playing basketball.");
-            Console.WriteLine("My favorite NBA player is Steph Curry.");
-            Console.WriteLine("I can solve 8 different types of Rubik's Cubes.");
+            Console.WriteLine("1. I am a senior in highschool.");
+            Console.WriteLine("2. I am 18 years old.");
+            Console.WriteLine("3. My ethnicity is Filipino.");
+            Console.WriteLine("4. My favorite academic subject is math.");
+            Console.WriteLine("5. I like to play videogames.");
+            Console.WriteLine("6. My favorite artist is Bruno Mars.");
+            Console.WriteLine("7. I enjoy playing volleyball.");
+            Console.WriteLine("8. I also enjoy watching and playing basketball.");
+            Console.WriteLine("9. My favorite NBA player is Steph Curry.");
+            Console.WriteLine("10. I can solve 8 different types of Rubik's Cubes.");
+            Console.WriteLine("11. I have a huge LEGO car collection of about 12 cars.");
+            Console.WriteLine("12. My favorite superhero is Spider-Man.");
+            Console.WriteLine("13. My current favorite movie is Marty Supreme.");
+            Console.WriteLine("14. My favorite movie all-time is Forest Gump.");
+            Console.WriteLine("15. I have been on two different cruise.");
+            Console.WriteLine("16. My favorite place I have traveled is Alaska.");
+            Console.WriteLine("17. I am in a family of 5");
+            Console.WriteLine("18. My favorite animes are Demon Slayer and Attack on Titan.");
+            Console.WriteLine("19. My favorite soda is Fanta.");
+            Console.WriteLine("20. I either want to go to UCLA or CalPoly SLO after high school.");
         }
     }
 }
