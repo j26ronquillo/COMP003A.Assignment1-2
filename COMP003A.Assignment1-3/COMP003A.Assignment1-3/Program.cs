feacfae17e4caa6fd96ee933ba5ed77b6957efd0
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-16 15:26:46 -0800

    added items 1-10

--- COMP003A.Assignment1-3/COMP003A.Assignment1-3/Program.cs
+++ COMP003A.Assignment1-3/COMP003A.Assignment1-3/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace COMP003A.Assignment1_3
+﻿using Microsoft.VisualBasic;
+
+namespace COMP003A.Assignment1_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me");
+            Console.WriteLine("I am a senior in highschool.");
+            Console.WriteLine("I am 18 years old.");
+            Console.WriteLine("My ethnicity is Filipino.");
+            Console.WriteLine("My favorite academic subject is math.");
+            Console.WriteLine("I like to play videogames.");
+            Console.WriteLine("My favorite artist is Bruno Mars.");
+            Console.WriteLine("I enjoy playing volleyball.");
+            Console.WriteLine("I also enjoy watching and playing basketball.");
+            Console.WriteLine("My favorite NBA player is Steph Curry.");
+            Console.WriteLine("I can solve 8 different types of Rubik's Cubes.");
         }
     }
 }
