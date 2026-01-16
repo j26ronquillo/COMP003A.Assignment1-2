68b2c64b813fa5d252e52a2a5cafee78a6f86b24
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-16 15:20:08 -0800

    Made the title (test commit)

---
+++ COMP003A.Assignment1-3/COMP003A.Assignment1-3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.Assignment1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+        }
+    }
+}
